8dec37c69212aa7f1ce186b2a87d0c4f3324be71
russ32 <rs_n@list.ru>
2016-04-18 17:07:05 +0300

    6. article/urls.py article.html

--- firstapp/article/urls.py
+++ firstapp/article/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
                        url(r'^3/', 'article.views.template_three_simple'),
                        url(r'^articles/all/$', 'article.views.articles'),
                        url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
-                       url(r'^', 'article.views.articles'),
+                       url(r'^$', 'article.views.articles'),
 
                        )
