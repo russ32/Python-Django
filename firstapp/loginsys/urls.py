4cf787279565393ebf728102d9ea21ed964530bd
russ32 <rs_n@list.ru>
2017-03-21 20:41:46 +0200

    11. Templates register users

--- firstapp/loginsys/urls.py
+++ firstapp/loginsys/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
 
                        url(r'^login/$', 'loginsys.views.login'),
                        url(r'^logout/$', 'loginsys.views.logout'),
+                       url(r'^register/$', 'loginsys.views.register'),
 
                        )
--- firstapp/loginsys/views.py
+++ firstapp/loginsys/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render_to_response, redirect
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.template.context_processors import csrf
 
 def login(request):
@@ -27,3 +28,17 @@ def logout(request):
     auth.logout(request)
     return redirect("/")
 
+def register(request):
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password2'])
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            args['form'] = newuser_form
+    return render_to_response('register.html', args)
